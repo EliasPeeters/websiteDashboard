4587389e0d72df2a636cab704c64abf0605664ce
Elias Peeters <Elias.peeters@otto.de>
2021-07-20 16:15:12 +0200

    Added database connection

--- websiteDashboard.js
+++ websiteDashboard.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const fs = require('fs');
+const mysql = require('mysql')
+
+
+// read credentials
+let credentials = JSON.parse(fs.readFileSync('credentials.json'));
+console.log('Reading credentials successful')
+
+connection = mysql.createConnection({
+	host: 'server.eliaspeeters.de',
+	user: credentials.database.username,
+	password: credentials.database.password,
+	database: 'website'
+});
+
+connection.connect((err) => {
+	if (err) {
+		console.log(err)
+        return false
+	} else {
+		console.log('Conntected to Database');
+        return true
+	}
+});
+
 
 app = express();
 
